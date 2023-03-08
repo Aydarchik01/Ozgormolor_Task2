86bc66797da528ad019cb15e9ef3ace77633183d
beka3301 <bekturhanazarov98@gmail.com>
2023-03-08 20:22:48 +0600

    upgrade commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Азамат");
-        System.out.println("      ойногону");
-        System.out.println("              кетти");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(" a = ");
+        int a = scanner.nextShort();
+        System.out.print(" b = ");
+        int b = scanner.nextShort();
+        System.out.print(" a = ");
+        int c = scanner.nextShort();
+        int sum = a + b + c;
+        int sum1 = a * b * c;
+        double sum2 = a + b + c;
+        System.out.println(a + "+" + b + "+" + c + "= " + sum);
+        System.out.println(a + "*" + b + "*" + c + "= " + sum1);
+        System.out.println("(" + a + "+" + b + "+" + c + ")" + "/3" + "= " + sum2 / 3);
     }
 }
